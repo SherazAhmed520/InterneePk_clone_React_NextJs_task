b74913b59cff24735db0026dba05f42b6c41d3e2
SherazAhmed520 <ayaz30672@gmail.com>
2024-02-10 23:30:34 +0500

    Update Internee.pk project added list

---
+++ public/int_List.js
@@ -0,0 +1,49 @@
+
+export const int_List = [
+    {
+        id: 1,
+        internshipName: 'Data Analyst',
+        imageUrl: 'https://www.internee.pk/images/jobs/analysis.jpg'
+    },
+    {
+        id: 2,
+        internshipName: 'Cyber Security',
+        imageUrl: 'https://www.internee.pk/images/jobs/hack.jpeg'
+    },
+    {
+        id: 3,
+        internshipName: 'Cloud Computing',
+        imageUrl: 'https://www.internee.pk/images/jobs/cloud.jpg'
+    },
+    {
+        id: 4,
+        internshipName: 'Frontend Development',
+        imageUrl: 'https://www.internee.pk/images/jobs/FrontEnd.jpeg'
+    },
+    {
+        id: 5,
+        internshipName: 'Backend Development',
+        imageUrl: 'https://www.internee.pk/images/jobs/BackendDevelopment.jpeg'
+    },
+    {
+        id: 6,
+        internshipName: 'Chatbot Development',
+        imageUrl: 'https://www.internee.pk/images/jobs/chatbotDevelopment.jpeg'
+    },
+    {
+        id: 7,
+        internshipName: 'Machine Learning',
+        imageUrl: 'https://www.internee.pk/images/jobs/Machine%20Learning.jpeg'
+    },
+    {
+        id: 8,
+        internshipName: 'Mobile App Development',
+        imageUrl: 'https://www.internee.pk/images/jobs/Mobile%20App%20Developer.jpeg'
+    },
+    {
+        id: 9,
+        internshipName: 'Graphic Design',
+        imageUrl: 'https://www.internee.pk/images/jobs/logo-designer-working-computer-desktop.jpg'
+    },
+
+]
